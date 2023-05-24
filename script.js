adc63c9288990215d57db176277a014b503eac0f
Geovani-Milian <134555933+Geovani-Milian@users.noreply.github.com>
2023-05-24 15:55:10 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const ATTEMPTS = 10;
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+  let userGuess = parseInt(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Números anteriores: ';
+  }
+  guesses.textContent += userGuess + ' ';
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = '¡Felicidades! ¡Adivinaste el número!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === ATTEMPTS) {
+    lastResult.textContent = '¡Perdiste! El número era ' + randomNumber;
+    lastResult.style.backgroundColor = 'red';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Incorrecto';
+    lastResult.style.backgroundColor = 'orange';
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = 'El número es más alto';
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = 'El número es más bajo';
+    }
+  }
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Iniciar un nuevo juego';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (let i = 0; i < resetParas.length; i++) {
+    resetParas[i].textContent = '';
+  }
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
